4120ad00d8be60b325e3b9b38eb4dd88c12542cf
Owen O'Malley <omalley@apache.org>
2019-11-26 09:57:16 -0800

    BUG-123823 ORC-27: Add support for proleptic Gregorian calendar for better support of dates before 1600AD.
    Signed-off-by: Owen O'Malley <omalley@apache.org>
    (cherry picked from commit 3b39b2708ecc2dc7b8ebf264a9edede8f7b70d57)
    
    Change-Id: I362812b5d62851bed61f8cba9048c9b4d701ff92
    (cherry picked from commit 7076f9ae341856a6bf6f2fb15a3c367897da5ac8)

--- java/core/src/test/org/apache/orc/impl/TestRecordReaderImpl.java
+++ java/core/src/test/org/apache/orc/impl/TestRecordReaderImpl.java
@@ -2019,7 +2019,7 @@ public class TestRecordReaderImpl {
             .end().build();
     RecordReaderImpl.SargApplier applier =
         new RecordReaderImpl.SargApplier(sarg, 1000, evolution,
-            OrcFile.WriterVersion.ORC_135, false);
+            OrcFile.WriterVersion.ORC_135, false, false, false);
     OrcProto.StripeInformation stripe =
         OrcProto.StripeInformation.newBuilder().setNumberOfRows(4000).build();
     OrcProto.RowIndex[] indexes = new OrcProto.RowIndex[3];
@@ -2067,7 +2067,7 @@ public class TestRecordReaderImpl {
             .end().build();
     RecordReaderImpl.SargApplier applier =
         new RecordReaderImpl.SargApplier(sarg, 1000, evolution,
-            OrcFile.WriterVersion.ORC_135, false);
+            OrcFile.WriterVersion.ORC_135, false, false, false);
     OrcProto.StripeInformation stripe =
         OrcProto.StripeInformation.newBuilder().setNumberOfRows(3000).build();
     OrcProto.RowIndex[] indexes = new OrcProto.RowIndex[3];
@@ -2114,7 +2114,8 @@ public class TestRecordReaderImpl {
         .equals("y", PredicateLeaf.Type.BOOLEAN, true).end().build();
 
     RecordReaderImpl.SargApplier applier =
-        new RecordReaderImpl.SargApplier(sarg, 1000, evo, OrcFile.WriterVersion.ORC_135, false);
+        new RecordReaderImpl.SargApplier(sarg, 1000, evo,
+            OrcFile.WriterVersion.ORC_135, false, false, false);
 
     OrcProto.StripeInformation stripe =
         OrcProto.StripeInformation.newBuilder().setNumberOfRows(2000).build();
@@ -2130,8 +2131,8 @@ public class TestRecordReaderImpl {
     encodings.add(OrcProto.ColumnEncoding.newBuilder().setKind(OrcProto.ColumnEncoding.Kind.DIRECT).build());
 
     boolean[] rows = applier.pickRowGroups(new ReaderImpl.StripeInformationImpl(stripe),
-            indexes, null, encodings, null, false);
-    assertEquals(SargApplier.READ_ALL_RGS, rows); //cannot filter for new column, return all rows
+        indexes, null, encodings, null, false);
+    assertEquals(RecordReaderImpl.SargApplier.READ_ALL_RGS, rows); //cannot filter for new column, return all rows
   }
 
   private boolean[] includeAll(TypeDescription readerType) {
